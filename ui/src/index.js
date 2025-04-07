2da56654955ac7ae9362b6b9cf6cc566745b1f5b
Jangjjae Lee <jangjae.lee@musinsa.com>
2025-04-07 10:46:51 +0900

    update

--- ui/src/index.js
+++ ui/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client'
 import RolloutConvert from './App.js'
 
 
+/*
 ((window: any) => {
+})(window);
+*/
+
+(window?.extensionsAPI?.registerResourceExtension) ?
   window?.extensionsAPI?.registerResourceExtension(
     RolloutConvert,
     'apps',
     'Deployment',
     'Rollout Convert',
-    {icon: 'fad fa-exchange'}
-  );
-})(window);
+    { icon: 'fad fa-exchange' }
+  ) : console.warn('extensionsAPI not found');
