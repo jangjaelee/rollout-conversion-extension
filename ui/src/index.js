e89229897dbdbd3db4bbd051510254d229ba7b83
Jangjjae Lee <jangjae.lee@musinsa.com>
2025-04-07 10:34:33 +0900

    update

--- ui/src/index.js
+++ ui/src/index.js
@@ -6,17 +6,11 @@ import RolloutConvert from './App.js'
 
 
 ((window: any) => {
-(window?.extensionsAPI?.registerResourceExtension) ?  
   window?.extensionsAPI?.registerResourceExtension(
     RolloutConvert,
     'apps',
     'Deployment',
     'Rollout Convert',
     {icon: 'fad fa-exchange'}
-  ) : ReactDOM.createRoot(document.getElementById('root')).render(
-      <React.StrictMode>
-        <RolloutConvert />
-      </React.StrictMode>,
   );
-
 })(window);
