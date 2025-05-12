de6dc4bf88f6525c758959cdfc04bd5410baf26b
Jangjae Lee <jangjae.lee@musinsa.com>
2025-05-12 10:36:42 +0900

    feature update - analysisTemplate

--- ui/src/utils/createAnalysisTemplate.js
+++ ui/src/utils/createAnalysisTemplate.js
@@ -7,28 +7,51 @@ export const createAnalysisTemplate = ({ name, namespace }) => {
       apiVersion: 'argoproj.io/v1alpha1',
       kind: 'AnalysisTemplate',
       metadata: {
-        name: templateName,
-        namespace: namespace,
         labels: {
           'created-by': 'rollout-conversion-extension',
         },
+        name: templateName,
+        namespace: namespace,
       },
       spec: {
-        metrics: [
-          {
-            name: 'success-rate',
-            interval: '30s',
-            count: 3,
-            successCondition: "result.status == 'Success'",
-            failureLimit: 1,
-            provider: {
-              prometheus: {
-                address: 'http://prometheus-operated.monitoring.svc:9090', // 수정 가능
-                query: 'sum(rate(http_requests_total{status=~"5.."}[2m])) / sum(rate(http_requests_total[2m])) < 0.05',
-              },
+        args: [
+        {
+          name: 'service-name',
+        },
+        {
+          name: 'prometheus-address',
+          value: 'http://mimir-proxy.mimir/prometheus',
+        },
+      ],
+      metrics: [
+        {
+          name: 'success-rate',
+          interval: '30s',
+          count: 3,
+          successCondition: 'result[0] >= 0.95',
+          failureLimit: 3,
+          inconclusiveLimit: 1,
+          provider: {
+            prometheus: {
+              address: '{{args.prometheus-address}}',
+              query: `
+                sum(rate(
+                  istio_requests_total{
+                    reporter="source",
+                    destination_service=~"{{args.service-name}}",
+                    response_code!~"5.*"
+                  }[5m]
+                )) /
+                sum(rate(
+                  istio_requests_total{
+                    reporter="source",
+                    destination_service=~"{{args.service-name}}"
+                  }[5m]
+                ))`,
             },
           },
-        ],
-      },
-    };
+        },
+      ],
+    },
   };
+};
